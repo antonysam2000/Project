a09487c40f3dbdd6529edcfe9d4a3c8ab373676b
Antony Samuel <antonysam2000@gmail.com>
2020-02-21 22:54:54 -0700

    First Commit

---
+++ Project/Project.cpp
@@ -0,0 +1,19 @@
+﻿// Project.cpp : Defines the entry point for the application.
+//
+
+#include "Project.h"
+
+int main()
+{
+	std::string val;
+	std::cout << "Type 'exit' to exit: ";
+	std::cin >> val;
+
+	while (val.compare("exit"))
+	{
+		std::cout << "Looped: ";
+		std::cin >> val;
+	}
+
+	std::cout << "\nExited\n";
+}
---
+++ out/build/WSL-Debug/Project/CMakeFiles/project-cmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project-cmake.dir/Project.cpp.o"
+  "project-cmake.pdb"
+  "project-cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project-cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
